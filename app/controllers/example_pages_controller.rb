f8e9044af2b225274efa064c678fa890eaf1a3c7
alexrcenteno <alex.centeno1991@gmail.com>
2023-01-13 13:57:33 -0600

    created example pages on rails

--- app/controllers/example_pages_controller.rb
+++ app/controllers/example_pages_controller.rb
@@ -2,4 +2,12 @@ class ExamplePagesController < ApplicationController
   def hello_method
     render json: { message: "hello" }
   end
+
+  def goodbye_method
+    render json: { message: "goodbye" }
+  end
+
+  def awesome_method
+    render json: { message: "awesome" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/hello_path", controller: "example_pages", action: "hello_method"
+  get "/goodbye_path", controller: "example_pages", action: "goodbye_method"
+  get "/awesome_path", controller: "example_pages", action: "awesome_method"
 end
